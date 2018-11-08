3693e934c457c8f810ee14f403fc5c1c05d69661
April-BOY <2111965204@qq.com>
2018-11-08 20:30:20 +0800

    modify README.md

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,16 @@
 </template>
 
 <script>
-import Mheader from "./components/m-header/m-header";
-
+/* 
+ * 这里的 MHeader 的 H 必须大写，m 可以大写，也可以小写没影响。
+ * 如果 H 没有大写，那么就无法生成 <m-header> 标签。
+ * 因为这个标签是根据驼峰命名来转化的
+ *
+*/
+import MHeader from "./components/m-header/m-header.vue";
 export default {
   components: {
-    Mheader
+    MHeader
   }
 };
 </script>
